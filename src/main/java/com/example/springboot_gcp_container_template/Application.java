0901fa65e20fdc86e6352ba462ee6e85d06d2354
MIURA, Kazuto <hiiamkazuto@gmail.com>
2025-10-26 16:41:52 +0900

    動作確認用エンドポイントの追加

--- src/main/java/com/example/springboot_gcp_container_template/Application.java
+++ src/main/java/com/example/springboot_gcp_container_template/Application.java
@@ -2,12 +2,27 @@ package com.example.springboot_gcp_container_template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/")
+	public HelloResponse hello() {
+		log.info("Hello, Log!");
+		return new HelloResponse("Hello, Spring Boot!");
+	}
+
+	public record HelloResponse(String message) {
 	}
 
 }
